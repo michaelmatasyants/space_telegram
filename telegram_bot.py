9470e1694b9ecd5cfcd5d1bdf95b903b8bf12058
Michael Matasyants <michael.matasyanc@gmail.com>
2023-02-27 23:46:35 +0300

    Publish photo to Telegram chanel

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,9 +5,11 @@ from spacex_nasa_api import get_api_key
 def main():
     bot = telegram.Bot(token=get_api_key()['tg_api_key'])
     updates = bot.get_updates()
-    CHAT_ID = updates[0]['channel_post']['chat']['id']
-    text_to_publish = 'Fist message published by bot'
-    bot.send_message(text=text_to_publish, chat_id=CHAT_ID)
+    chat_id = updates[0]['channel_post']['chat']['id']
+    bot.send_message(chat_id=chat_id, text='Fist message published by bot')
+    bot.send_document(chat_id=chat_id, document=open(
+        './images/spacex_0.jpg', 'rb'
+        ))
 
 
 if __name__ == '__main__':
