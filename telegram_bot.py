b5050a1f13fc88cca831b7f77558eb6a3175c664
Michael Matasyants <michael.matasyanc@gmail.com>
2023-02-28 18:10:28 +0300

    Split into 2 functions, publish_message() and publish_image_as_file()

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,15 +2,15 @@ import telegram
 from spacex_nasa_api import get_api_key
 
 
-def main():
+def publish_message(text):
     bot = telegram.Bot(token=get_api_key()['tg_api_key'])
     updates = bot.get_updates()
     chat_id = updates[0]['channel_post']['chat']['id']
-    bot.send_message(chat_id=chat_id, text='Fist message published by bot')
-    bot.send_document(chat_id=chat_id, document=open(
-        './images/spacex_0.jpg', 'rb'
-        ))
+    bot.send_message(chat_id=chat_id, text=text)
 
 
-if __name__ == '__main__':
-    main()
+def publish_image_as_file(photo_path):
+    bot = telegram.Bot(token=get_api_key()['tg_api_key'])
+    updates = bot.get_updates()
+    chat_id = updates[0]['channel_post']['chat']['id']
+    bot.send_document(chat_id=chat_id, document=open(photo_path, 'rb'))
