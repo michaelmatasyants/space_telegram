997ec077ecb9325272c13cca5f6abe024d81f512
Michael Matasyants <michael.matasyanc@gmail.com>
2023-03-04 17:27:30 +0300

    Make the functions suitable for telegram bot as well

--- spacex_nasa_api.py
+++ spacex_nasa_api.py
@@ -4,6 +4,7 @@ from urllib.parse import urlparse, unquote
 from pathlib import Path
 from io import BytesIO
 from PIL import Image
+import telegram
 
 
 def get_filename_extension(image_url):
@@ -14,7 +15,12 @@ def get_filename_extension(image_url):
 
 def get_api_key():
     load_dotenv(find_dotenv())
-    return os.environ["NASA_API_KEY"]
+    api_keys = {
+        'nasa_api_key': os.environ['NASA_API_KEY'],
+        'tg_api_key': os.environ['TG_TOKEN'],
+        'chat_id': os.environ['CHAT_ID']
+        }
+    return api_keys
 
 
 def check_create_path(path_to_save):
@@ -23,3 +29,14 @@ def check_create_path(path_to_save):
 
 def save_image(bytes_image, path_to_save, image_name="image.png"):
     Image.open(BytesIO(bytes_image)).save(f"{path_to_save}/{image_name}")
+
+
+def publish_message(text):
+    bot = telegram.Bot(token=get_api_key()['tg_api_key'])
+    bot.send_message(chat_id=get_api_key()['chat_id'], text=text)
+
+
+def publish_image_as_file(photo_path):
+    bot = telegram.Bot(token=get_api_key()['tg_api_key'])
+    bot.send_document(chat_id=get_api_key()['chat_id'],
+                      document=open(photo_path, 'rb'))
