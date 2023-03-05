1149c11474b57184d606190bf14d297b78dd7aae
Michael Matasyants <michael.matasyanc@gmail.com>
2023-03-05 11:26:24 +0300

    Pass chat_id via .env

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,16 +1,14 @@
 import telegram
-from spacex_nasa_api import get_api_key
+import spacex_nasa_api
 
 
 def publish_message(text):
-    bot = telegram.Bot(token=get_api_key()['tg_api_key'])
-    updates = bot.get_updates()
-    chat_id = updates[0]['channel_post']['chat']['id']
-    bot.send_message(chat_id=chat_id, text=text)
+    bot = telegram.Bot(token=spacex_nasa_api.get_api_key()['tg_api_key'])
+    bot.send_message(chat_id=spacex_nasa_api.get_api_key()['chat_id'],
+                     text=text)
 
 
 def publish_image_as_file(photo_path):
-    bot = telegram.Bot(token=get_api_key()['tg_api_key'])
-    updates = bot.get_updates()
-    chat_id = updates[0]['channel_post']['chat']['id']
-    bot.send_document(chat_id=chat_id, document=open(photo_path, 'rb'))
+    bot = telegram.Bot(token=spacex_nasa_api.get_api_key()['tg_api_key'])
+    bot.send_document(chat_id=spacex_nasa_api.get_api_key()['chat_id'],
+                      document=open(photo_path, 'rb'))
