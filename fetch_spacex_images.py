49722cef12ae1eac3e4a1b1bb538ccdbf2e3b2d0
Michael Matasyants <michael.matasyanc@gmail.com>
2023-03-23 13:47:31 +0300

    Specify the type of arguments

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -22,10 +22,10 @@ def main():
     )
     image_parser.add_argument('-l', '--launch_id', default='latest', type=str,
                               help='enter the launch id to download images.')
-    image_parser.add_argument('-p', '--path', default=Path('images'),
+    image_parser.add_argument('-p', '--path', default='images', type=Path,
                               help='enter path to save the image')
     args = image_parser.parse_args()
-    check_create_path(Path(args.path))
+    check_create_path(args.path)
 
     try:
         links_launch_images = get_links_to_photos(args.launch_id)
@@ -42,7 +42,7 @@ def main():
                 link_is_valid = False
                 image_quantity -= 1
             if link_is_valid:
-                save_image(response_image.content, Path(args.path),
+                save_image(response_image.content, args.path,
                            '{}_{}'.format(
                     args.launch_id, "".join(get_filename_extension(link)))
                     )
